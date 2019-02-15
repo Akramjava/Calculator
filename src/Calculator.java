b86361c6dd00a23022a5013f6182e3d154dc4cf3
Akramjava <akramttaa@gmail.com>
2019-02-15 01:07:50 -0800

    LOCAL: Add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,7 +11,8 @@ public class Calculator {
     public static double multiply(double num1, double num2){
         return num1 * num2;
 }
-    public static double add(double num3, double num4){
-        return num3+num4;
+    public static double divide(double num1, double num2) {
+        return num1/num2;
+        return result;
     }
 }
